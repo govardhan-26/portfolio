e08c948422afd08f8ed1e76b6230f1bb998a9622
govardhan-26 <bgrnaidu@gmail.com>
2023-06-24 01:31:15 +0530

    Home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -22,6 +22,10 @@ const Home = () => {
                 <a href="https://www.linkedin.com/in/govardhan26/" className='icon' target='_blanck'><img src={Linkedin} alt="Linkedin" /></a>
                 <a href="https://mail.google.com/mail/?view=cm&to=bgrnaidu%40gmail.com" className='icon' target='_blanck'><img src={Gmail} alt="Mail" /></a>
                 </div>
+                <div className='resume'>
+                    <div className='res'><a href="https://drive.google.com/file/d/13485mOstFxAK1VJiyPn6MquSAbRk7lTY/view?usp=sharing" target = "_blank">Resume</a></div>
+                    <div className='res'><a href="https://mail.google.com/mail/?view=cm&to=bgrnaidu%40gmail.com" target = "_blank">Hire Me</a></div>
+                </div>
             </div>
         </div>
         <div className='pic'>
