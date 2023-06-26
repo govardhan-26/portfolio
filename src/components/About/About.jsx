1ea4fe91899e9f3058c80592df3cf90b3e0c589f
govardhan-26 <bgrnaidu@gmail.com>
2023-06-27 04:14:52 +0530

    abt1

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,16 +4,17 @@ import './About.css'
 const About = () => {
   return (
     <div className='abt-Container'>
+        <h2 className='abt'>About Me</h2>
         <div className='abt-intro'>
-            <h2>About Me</h2>
-            <p className='intro'>Hi, I am Govardhan Rao Naidu Babbadi</p>
+            <p className='intro'>Hi, I am <b>Govardhan Rao Naidu Babbadi</b></p>
             <p className='intro'>I am 4th year Undergraduate <b>Btech</b> Student studying in <b>Indian Institute of Engineering Science and Technology, Shibpur</b>.</p>
             <p className='intro'>I am interested in Development of <b>Web Apps</b> and learning new things. </p>
-            <h2>Tech Stack</h2>
+            
+            <h2 className='tech'>Tech Stack</h2>
             <p className='intro'>
-              <p><b style={{fontSize: "larger"}}>Programming Languages</b> :  C, C++, Python, HTML, CSS, Javascript</p>
-              <p><b style={{fontSize: "larger"}}>Developer Tools</b> :  Git, Github, Visual Studio Code, Terminal</p>
-              <p><b style={{fontSize: "larger"}}>Frameworks</b> :  ReactJs, NodeJs, ExpressJs..</p>
+              <p><b style={{fontSize: "larger", color : "red"}}>Programming Languages</b> :  C, C++, Python, HTML, CSS, Javascript</p>
+              <p><b style={{fontSize: "larger", color : "red"}}>Developer Tools</b> :  Git, Github, Visual Studio Code, Terminal</p>
+              <p><b style={{fontSize: "larger", color : "red"}}>Frameworks</b> :  ReactJs, NodeJs, ExpressJs..</p>
             </p>                      
         </div>
     </div>
